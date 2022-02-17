eef0d70fb3fe8a712b0015bda677ce93518bad63
mcv_ <murat@visiosoft.com.tr>
2022-02-17 17:10:00 +0300

    test has added json export and new version

--- tests/test_gps_anomaly.py
+++ tests/test_gps_anomaly.py
@@ -2,7 +2,8 @@ import urllib.request
 import json
 from gps_anomaly.detector import Anomaly
 
-res = urllib.request.urlopen('https://raw.githubusercontent.com/mapilio/mapilio-kit/master/schema/image_description_schema.json')
+res = urllib.request.urlopen(
+    'https://raw.githubusercontent.com/mapilio/mapilio-kit/master/schema/image_description_schema.json')
 res_body = res.read()
 
 # https://docs.python.org/3/library/json.html
@@ -19,6 +20,7 @@ inforrmation = {"Information":
 
 json_file.append(inforrmation)
 anomaly = Anomaly()
-result, failure_images,_ = anomaly.anomaly_detector(frames=json_file)
+result, failure_images, _ = anomaly.anomaly_detector(frames=json_file)
 
-print(result)
+with open(file="image_description_schema_export.json", mode="w") as f:
+    json.dump(result, f, indent=4)
