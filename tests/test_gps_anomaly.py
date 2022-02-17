fa0952b1996a95a890bd6f672984c89c9c73f566
izzetbasli <izzet@visiosoft.com.tr>
2022-02-17 16:59:57 +0300

    test_case added

---
+++ tests/test_gps_anomaly.py
@@ -0,0 +1,24 @@
+import urllib.request
+import json
+from gps_anomaly.detector import Anomaly
+
+res = urllib.request.urlopen('https://raw.githubusercontent.com/mapilio/mapilio-kit/master/schema/image_description_schema.json')
+res_body = res.read()
+
+# https://docs.python.org/3/library/json.html
+json_file = json.loads(res_body.decode("utf-8"))
+
+inforrmation = {"Information":
+    {
+        "total_images": 7,
+        "processed_images": 7,
+        "failed_images": 0,
+        "duplicated_images": 0,
+    }
+}
+
+json_file.append(inforrmation)
+anomaly = Anomaly()
+result, failure_images,_ = anomaly.anomaly_detector(frames=json_file)
+
+print(result)
