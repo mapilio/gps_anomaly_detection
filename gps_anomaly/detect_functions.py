be32ec9d411ba6b17491a7348bc09eeb6d6689a7
gorkemgul <gorkemgtech@gmail.com>
2024-10-07 14:06:53 +0100

    update: add distance checker function to detect anomalies (duplication or fail)

--- gps_anomaly/detect_functions.py
+++ gps_anomaly/detect_functions.py
@@ -1,6 +1,7 @@
 from .one_sequence import Sequence
 import os
-import json
+import re
+from gps_anomaly.config.distance import Distance
 
 
 def first_point(anomaly_points, extracted_seq, seq):
@@ -34,9 +35,30 @@ class Info:
                     file_list.append(os.path.join(seq['filename']))
         return file_list
 
-    def update_info(self, info, file_names, uud):
-        info['Information']['processed_images'] = info['Information']['processed_images'] - len(file_names)
-        info['Information']['failed_images'] = info['Information']['failed_images'] + len(file_names)
+    def check_distance_from_desc(self, desc):
+        """
+        Checks the distance between images, marks as failed or duplicate.
+        """
+        if 'anomaly_reason' in desc:
+            reason = desc['anomaly_reason']
+            match = re.search(r'The distance:\s*([-+]?[0-9]*\.?[0-9]+)', reason)
+            if match:
+                distance = float(match.group(1))
+                if distance < Distance.distance_low_limit:
+                    return "duplicate"
+                else:
+                    return "failed"
+
+        return None
+
+    def update_info(self, descs, info, uud):
+        for desc in descs:
+            if self.check_distance_from_desc(desc) == "duplicate":
+                info["Information"]["duplicated_images"] += 1
+                info["Information"]["processed_images"] -= 1
+            elif self.check_distance_from_desc(desc) == "failed":
+                info["Information"]["failed_images"] += 1
+                info["Information"]["processed_images"] -= 1
         uud = [lis for lis in uud if lis != []]
         info['Information']['anomaly_sequences'] = uud
         return info
@@ -57,17 +79,17 @@ class Info:
         return united_sequences
 
 
-def mark_points(decs):
+def mark_points(desc):
     """
         - Group the sequences, remove that has one element sequences.
         - For each series, calculate distance of all points in the sequence.
         - using length anomaly/sequence ratio, get the final result sequences extracted anomalies
     """
-    data_all = Sequence(decs)
+    data_all = Sequence(desc)
     info = Info()
     extracted, information, anomaly, uud, with_anomaly, order_seq = data_all.groupy_to_result()
     filenames_list = info.create_list_filename(anomaly)
-    information = info.update_info(information, filenames_list, uud)
+    information = info.update_info(desc, information, uud)
     order_seq = info.create_json(order_seq, information)
     anomaly_points = info.create_json(anomaly, information)
     return order_seq, filenames_list, anomaly_points
